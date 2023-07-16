b56e4edcdb92b1f9e5e728c59e24c6ac9e0b91e4
shrey802 <shreyashpingle752@gmail.com>
2023-07-17 00:04:42 +0530

    some changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        customKey: 'he2jl8b9JSBTrxZp9QbCc708v5dJYm8srZcUatykMDtFEvblABo8gBMk',
+    },
+    images: {
+        domains: ['images.pexels.com', 'www.pexels.com', 'api.pexels.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/photos/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.pexels.com',
+                port: '',
+                pathname: '/photo/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.pexels.com',
+                port: '',
+                pathname: '/**'
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,167 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+/* eslint-disable react-hooks/exhaustive-deps */
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+'use client'
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { createClient } from "pexels";
+const client = createClient("he2jl8b9JSBTrxZp9QbCc708v5dJYm8srZcUatykMDtFEvblABo8gBMk");
+const perPage = 16;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+const Home = () => {
+  const [page, setPage] = useState(1);
+  const [pictures, setPictures] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const categories = ["Nature", "Travel", "Food", "Animals", "Architecture"];
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+  const loadMoreImages = async () => {
+    try {
+      setLoading(true);
+      const response: any = await client.photos.curated({ per_page: perPage, page: page + 1 });
+      const newPictures = response.photos.map((photo: any) => ({
+        ...photo,
+        category: categories[Math.floor(Math.random() * categories.length)], // Assign a random category to each image
+        liked: false, // Add the liked property and set it to false initially
+      }));
+      setPictures((prevPictures) => [...prevPictures, ...newPictures]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error("Error loading more images:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  const handleScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 10 && !loading) {
+      loadMoreImages();
+    }
+  };
+
+  const handleSearchInputChange = (event: any) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const response: any = await client.photos.search({ query: searchQuery, per_page: perPage });
+      const searchResults = response.photos.map((photo: any) => ({
+        ...photo,
+        category: categories[Math.floor(Math.random() * categories.length)], // Assign a random category to each image
+        liked: false, // Add the liked property and set it to false initially
+      }));
+      setPictures(searchResults);
+      setPage(1);
+      setSelectedCategory("");
+    } catch (error) {
+      console.error("Error searching images:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCategoryFilter = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const handleLike = (id: number) => {
+    setPictures((prevPictures) => {
+      return prevPictures.map((pic) => {
+        if (pic.id === id) {
+          return {
+            ...pic,
+            liked: !pic.liked, // Toggle the liked property
+          };
+        }
+        return pic;
+      });
+    });
+  };
+
+  const filteredPictures = selectedCategory
+    ? pictures.filter((pic) => pic.category === selectedCategory)
+    : pictures;
+
+  useEffect(() => {
+    const fetchPictures = async () => {
+      const response: any = await client.photos.curated({ per_page: perPage });
+      const initialPictures = response.photos.map((photo: any) => ({
+        ...photo,
+        category: categories[Math.floor(Math.random() * categories.length)], // Assign a random category to each image
+        liked: false, // Add the liked property and set it to false initially
+      }));
+      setPictures(initialPictures);
+    };
+    fetchPictures();
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [loading]);
+
+function reload() {
+  window.location.reload();
 }
+
+  useEffect(() => {
+    setPictures([]);
+    setPage(1);
+  }, [searchQuery]);
+
+  return (
+    <main>
+      <h1 style={{ textAlign: "center", color: "blue" }} onClick={reload}>Image Gallery</h1>
+      <form onSubmit={handleSearchSubmit}>
+        <input type="text" value={searchQuery} onChange={handleSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        <p>Filter by Category:</p>
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => handleCategoryFilter(category)}
+            style={{ marginRight: "10px" }}
+          >
+            {category}
+          </button>
+        ))}
+        <button onClick={() => handleCategoryFilter("")}>All</button>
+      </div>
+      <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+        {filteredPictures.map((pic: any) => (
+          <div key={pic.id} style={{ margin: "10px" }}>
+            <Image
+              src={pic.src.original}
+              alt={pic.alt}
+              height={200}
+              width={200}
+              onClick={() => handleLike(pic.id)} // Add the onClick event handler to toggle the liked property
+              style={{ cursor: "pointer" }} // Add cursor style to indicate the image is clickable
+            />
+            <p>Category: {pic.category}</p>
+            <p>
+            Liked:{" "}
+              <span style={{ color: pic.liked ? "green" : "red" }}>
+                {pic.liked ? "Yes" : "No"}
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+      {loading && <p>Loading more images...</p>}
+    </main>
+  );
+};
+
+export default Home;
+
